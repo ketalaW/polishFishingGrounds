40975ee019182cc84598119238c48601978047e7
Michał Walatek <michal.walatek@almar.pl>
2025-01-27 20:23:23 +0100

    Zmiana pozycji startowej

--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,17 @@ const game = new ex.Engine({
 	pixelRatio: 2,
 	displayMode: ex.DisplayMode.FillScreen,
 	pointerScope: ex.PointerScope.Document,
+	width: 800,
+	height: 600,
 });
+
+game.screen.applyResolutionAndViewport();
 
 const hero = new Hero();
 game.add(hero);
 const loader = new ex.Loader(Object.values(Resources));
+game.currentScene.camera.pos = new ex.Vector(-600, -300);
 
 game.start(loader).then(() => {
-	Resources.Map.addToScene(game.currentScene), { pos: ex.vec(100, 100) };
+	Resources.Map.addToScene(game.currentScene, { pos: ex.vec(2500, 0) });
 });
